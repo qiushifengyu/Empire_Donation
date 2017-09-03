f9801985e22dbfeb988b2d8a38077976243ed575
Matt Homjxie _ <jihadLkmaty218@users.noreply.github.com>
2017-09-03 09:59:21 -0400

    Update ScriptBlock.py

--- ScriptBlock.py
+++ ScriptBlock.py
@@ -5,32 +5,35 @@ class Module:
     def __init__(self, mainMenu, params=[]):
 
         self.info = {
-            'Name': 'Bypass Script_Block',
+            'Name': 'ScriptBlock Bypass',
 
-            'Author': ['@Homjxie:>' 'Ryan_Cobb',],
+            'Author': ['@Homjxie:>', 'Ryan_Cobb',],
 
             'Description': ("@Ryan Cobb @Homjxie"
                             "Most companies only realize the need to enable script block logging after it is too late. To provide some recourse in this situation, PowerShell automatically logs script blocks when they have content often used by malicious scripts."),
+
             'Background' : True,
 
             'OutputExtension' : None,
-            
+
             'NeedsAdmin' : True,
+
+            'OpsecSafe' : True,
 
             'Language' : 'powershell',
 
             'MinLanguageVersion' : '2',
-            
+
             'Comments': [
                 'https://twitter.com/cobbr_io',
-                'https://twitter.com/GihadAlkmaty',
+                'https://twitter.com/homjxi0e',
             ]
         }
 
          
         self.options = {
-            # format:
-            #   value_name : {description, required, default_value}
+            
+            
             'Agent' : {
                 'Description'   :   'Agent to run module on.',
                 'Required'      :   True,
@@ -38,22 +41,22 @@ class Module:
             }
         }
 
-        
+       
         self.mainMenu = mainMenu
-        
+
         for param in params:
-             
+          
             option, value = param
             if option in self.options:
                 self.options[option]['Value'] = value
 
 
-def generate(self):
+    def generate(self):
 
         moduleName = self.info["Name"]
 
          
-        moduleSource = self.mainMenu.installPath + "/data/module_source/management/Bypass-ScriptBlock.ps1"
+        moduleSource = self.mainMenu.installPath + "/data/module_source/management/ScriptBlock.ps1"
 
         try:
             f = open(moduleSource, 'r')
