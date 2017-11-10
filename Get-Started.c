e21753addeae0ea106ca36a1174c8c0d2bc31ec0
Matt homjxi0e <jihadLkmaty218@users.noreply.github.com>
2017-11-10 03:05:48 +0200

    Add files via upload

---
+++ Get-Started.c
@@ -0,0 +1,10 @@
+#> this subject About Facilitate my infrastructure 
+#> to Users Empire-Powershell
+#> Made in a unique and easy way without any fatigue //man 
+cd ..
+cd ..
+echo "Home you/User"
+read targets
+echo "Home you/User2"
+read target 
+echo empire "()" "{ cd /$targets/Desktop/Empire/; /$target/Desktop/Empire/./empire; }">>.bashrc
