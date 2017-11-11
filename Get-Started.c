c5a4c49e8213d4adab3138a686f5984f0377b061
Matt homjxi0e <jihadLkmaty218@users.noreply.github.com>
2017-11-11 10:07:27 -0500

    Update Get-Started.c

--- Get-Started.c
+++ Get-Started.c
@@ -1,11 +1,38 @@
-#> this subject About Facilitate my infrastructure 
+GN=$(tput setaf 9 tput blood)
+function Get-Started {
+
+#> Author @matt homjxi0e
+
+#> this subject About Facilitate my infrastructure
+
 #> to Users Empire-Powershell
-#> Made in a unique and easy way without any fatigue //man 
-#> path this file Is in File Empire //
-cd ..
-cd ..
-echo "Home you/User"
+
+#> Made in a unique and easy way without any fatigue //man
+$back cd ..
+
+$back cd ..
+
+# remove file-.bashrc It can be left from anywhere else
+
+$remove rm .bashrc
+
+# Method read to invoke-empire
+
+echo "home you/User"
+
+#Method read to invoke-empire
+
 read targets
-echo "Home you/User2"
-read target 
-echo empire "()" "{ cd /$targets/Desktop/Empire/; /$target/Desktop/Empire/./empire; }">>.bashrc
+
+echo "home you/User2"
+
+read target
+
+ echo empire "()" "{ cd /$targets/Desktop/Empire/; /$target/Desktop/Empire/./empire; }">>.bashrc
+(
+   echo $GN "[*] complete-Name the invoke is:>  empire";
+ sleep 2.22;
+)
+
+}
+Get-Started
